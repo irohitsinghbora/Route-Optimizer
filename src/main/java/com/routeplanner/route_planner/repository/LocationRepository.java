77592e537653f63014031dca4fb97b82a426f569
Rohit Singh Bora <irohitsinghbora@gmail.com>
2026-05-29 09:24:21 +0530

    Delete src directory

--- src/main/java/com/routeplanner/route_planner/repository/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.routeplanner.route_planner.repository;
-
-import com.routeplanner.route_planner.model.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository<Location, Long> {
-}
